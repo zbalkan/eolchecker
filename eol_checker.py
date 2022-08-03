939eb1dc07e460f238d218999fbb8fa1c953cafa
Zafer Balkan <zafer@zaferbalkan.com>
2022-08-03 21:44:26 +0300

    Managed to parse and deserialize EOL software list

--- eol_checker.py
+++ eol_checker.py
@@ -2,15 +2,41 @@ import json
 
 import requests
 
+from softwareLifecycle import SoftwareLifeCycle
+
+
+class EOLChecker:
+
+    SOFTWARE_EOL_API: str = 'https://endoflife.date'
+
+    eolSoftware: list[SoftwareLifeCycle]
+
+    def __init__(self) -> None:
+        self.eolSoftware = self.get_eol_software()
+
+    def get_eol_software(self) -> list[SoftwareLifeCycle]:
+        softwareList: list[SoftwareLifeCycle] = []
+
+        softwareListResponse: requests.Response = requests.get(
+            self.SOFTWARE_EOL_API + "/api/all.json")
+        softwareListJsonString: list[str] = json.loads(softwareListResponse.content)
+        for softwareName in softwareListJsonString:
+            softwareResponse: requests.Response = requests.get(
+                self.SOFTWARE_EOL_API + "/api/" + softwareName + ".json")
+            softwareJsonString: list[str] = json.loads(softwareResponse.content)
+            for softwareLifecycleJson in softwareJsonString:
+                temp: SoftwareLifeCycle = SoftwareLifeCycle.from_dict(softwareLifecycleJson)
+                temp.name = softwareName
+                softwareList.append(temp)
+
+        return softwareList
+
 
 def main() -> None:
 
-    response: requests.Response = requests.get(
-        "https://endoflife.date/api/all.json")
-    val: bytes = response.content
-    products: list[str] = json.loads(val)
-    for product in products:
-        print(product)
+    checker: EOLChecker = EOLChecker()
+    for software in checker.eolSoftware:
+        print(software)
 
 
 if __name__ == '__main__':
